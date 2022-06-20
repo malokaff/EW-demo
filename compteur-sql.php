bbbe7b87e18566b45e4b761470a92444cdf87346
malokaff <benjamin.maze@gmail.com>
2022-06-20 16:57:00 +0200

    remove pwd

--- compteur-sql.php
+++ compteur-sql.php
@@ -1,8 +1,10 @@
+
 <br>
 <?php
+include("password.php");
 
 //read from database
-$mysqli = new mysqli("10.29.21.15", "root", "Pensando0$", "MQTT");
+$mysqli = new mysqli("10.29.21.15", "root", "$pwd_sql", "MQTT");
 if ($mysqli->connect_errno) {
 	echo "<font color='FF000000' size='14'><b>";
 	echo "failed to connect to database :"; 
